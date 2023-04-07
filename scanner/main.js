bdf1f61dd755f476771d33f0e37fd2b9538cf675
diasgc <gustavocdias@me.com>
2023-04-07 16:42:26 +0100

    upd

--- scanner/main.js
+++ scanner/main.js
@@ -6,13 +6,11 @@ var lastResult, countResults = 0;
 function onScanSuccess(decodedText, decodedResult) {
     if (decodedText !== lastResult) {
       beep.play();
-      let row = table.insertRow(0);
-      let c1 = row.insertCell(0);
-      let c2 = row.insertCell(1);
-      let c3 = row.insertCell(2);
-      c1.innerHTML = countResults;
-      c2.innerHTML = decodedText.length;
-      c3.innerHTML = decodedText;
+      let row = table.insertRow(countResults + 1);
+      row.insertCell(0).innerHTML = countResults;
+      row.insertCell(1).innerHTML = decodedText.length;
+      row.insertCell(2).innerHTML = "0x" + decodedText.charCodeAt(0).toString(16);
+      row.insertCell(3).innerHTML = decodedText;
       ++countResults;
       lastResult = decodedText;
       // Handle on success condition with the decoded message.
