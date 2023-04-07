338d8ec525e368e0b5d2540d6240ffca96848705
diasgc <gustavocdias@me.com>
2023-04-07 15:48:21 +0100

    upd

--- scanner/main.js
+++ scanner/main.js
@@ -1,23 +1,28 @@
 const beep = document.getElementById('beep');
+const table = document.getElementById("table-results");
 const resultContainer = document.getElementById('qr-reader-results');
 var lastResult, countResults = 0;
 
 function onScanSuccess(decodedText, decodedResult) {
     if (decodedText !== lastResult) {
       beep.play();
+      let row = table.insertRow(0);
+      let c1 = row.insertCell(0);
+      let c2 = row.insertCell(1);
+      let c2 = row.insertCell(2);
+      c1.innerHTML = countResults;
+      c2.innerHTML = decodedText.length;
+      c3.innerHTML = decodedText;
       ++countResults;
       lastResult = decodedText;
       // Handle on success condition with the decoded message.
       console.log(`Scan result ${decodedText}`, decodedResult);
-      resultContainer.innerText = decodedText;
-      html5QrcodeScanner.stop().then((ignore) => {
-        // QR Code scanning is stopped.
-      }).catch((err) => {
-        // Stop failed, handle it.
-      });
+      //resultContainer.innerText = decodedText;
+      html5QrcodeScanner.clear();
+      html5QrcodeScanner.pause();
     }
 }
 
-var html5QrcodeScanner = new Html5QrcodeScanner(
+let html5QrcodeScanner = new Html5QrcodeScanner(
     "qr-reader", { fps: 10, qrbox: 250 });
 html5QrcodeScanner.render(onScanSuccess);
