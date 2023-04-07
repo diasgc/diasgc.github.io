61d87143d1450e2511c16973a8f7f200e3c6e571
diasgc <gustavocdias@me.com>
2023-04-07 17:04:42 +0100

    upd

--- scanner/main.js
+++ scanner/main.js
@@ -2,15 +2,22 @@ const beep = document.getElementById('beep');
 const table = document.getElementById("table-results");
 const resultContainer = document.getElementById('qr-reader-results');
 var lastResult, countResults = 0;
+const gs = String.fromCharCode(29);
+
+function tryGTIN(s){
+  var out = s.split(gs);
+  return out.join('\n');
+}
 
 function onScanSuccess(decodedText, decodedResult) {
     if (decodedText !== lastResult) {
       beep.play();
       let row = table.insertRow(countResults + 1);
+      let fc = decodedText.charCodeAt(0);
       row.insertCell(0).innerHTML = countResults;
       row.insertCell(1).innerHTML = decodedText.length;
-      row.insertCell(2).innerHTML = "0x" + decodedText.charCodeAt(0).toString(16);
-      row.insertCell(3).innerHTML = decodedText;
+      row.insertCell(2).innerHTML = "0x" + fc.toString(16);
+      row.insertCell(3).innerHTML = decodedText.replaceAll(gs,'\n');
       ++countResults;
       lastResult = decodedText;
       // Handle on success condition with the decoded message.
