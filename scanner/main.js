aadd903b8c51e2f608bfa0dfbc62a1a3f6186ac4
diasgc <gustavocdias@me.com>
2023-04-07 19:19:22 +0100

    upd

--- scanner/main.js
+++ scanner/main.js
@@ -5,16 +5,27 @@ var lastResult, countResults = 0;
 const gs = String.fromCharCode(29);
 
 // test with var str = "010560031211263417250731102423821100923530526127145665971";
-function tryINF(str){
+function tryINF(str){ 
   var a, tag, arr = str.split(gs), out="";
-  for (a of arr){
+  while (arr.length > 0){
+      a = arr.pop();
       if (a.length < 2)
           continue;
       tag = a.slice(0,2);
       if (tag === "01"){
+        // 05600312112634
         out +="PC: " + a.slice(2,16) + "\n";
-        out +="VAL: 20" + a.substring(18,20)+"/"+a.substring(20,22) + "/" + a.substring(22,24) + "\n";
-        out +="LOT: " + a.substring(26) + "\n";
+        if (a.length > 16)
+          arr.push(a.substring(16));
+        continue;
+      } else if (tag === "17") {
+        out +="VAL: 20" + a.substring(2,4)+"/"+a.substring(4,6) + "/" + a.substring(6,8) + "\n";
+        if (a.length > 6)
+          arr.push(a.substring(8));
+        continue;
+      } else if (tag === "10") {
+        out +="LOT: " + a.substring(2) + "\n";
+        continue;
       } else if (tag === "21") {
         out += "SN: " + a.substring(2) + "\n";
       } else if (tag === "71") {
