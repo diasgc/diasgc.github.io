f31b884f4ad758497e64024351d6ba8ef7556921
diasgc <gustavocdias@me.com>
2023-04-09 19:27:06 +0100

    upd

--- lc/main.js
+++ lc/main.js
@@ -1,6 +1,13 @@
 async function playVideoFromCamera() {
     try {
-        const constraints = {'video': true, 'audio': true};
+        const constraints = {
+            'video': {
+                'deviceId': 0,
+                'width': {'min': 1280},
+                'height': {'min': 720}
+                }
+            }
+        };
         const stream = await navigator.mediaDevices.getUserMedia(constraints);
         const videoElement = document.querySelector('video#localVideo');
         videoElement.srcObject = stream;
