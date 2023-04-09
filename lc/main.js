be87566fd03e81ab995fc5e25b6e0d27e0ff777c
diasgc <gustavocdias@me.com>
2023-04-09 19:45:19 +0100

    upd

--- lc/main.js
+++ lc/main.js
@@ -5,10 +5,21 @@ async function playVideoFromCamera() {
                 'deviceId': 0,
                 'width': {'min': 720},
                 'height': {'min': 1280}
-                }
-            }
+                },
+            'audio': false
         };
         const stream = await navigator.mediaDevices.getUserMedia(constraints);
+        var track = stream.getVideoTracks()[0];
+        var caps = track.getCapabilities();
+        if (caps.zoom)
+            track.applyConstraints({
+                advanced: [
+                    {
+                        zoom: 2
+                    }
+                ]
+            });
+
         const videoElement = document.querySelector('video#localVideo');
         videoElement.srcObject = stream;
     } catch(error) {
