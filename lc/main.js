b4515be80ea881919793029058b4611e5bc9db3e
diasgc <15062495+diasgc@users.noreply.github.com>
2023-04-09 13:32:36 +0100

    lc-upd

--- lc/main.js
+++ lc/main.js
@@ -1,3 +1,4 @@
+// https://www.npmjs.com/package/html5-qrcode#pro-mode---if-you-want-to-implement-your-own-user-interface
 let config = {
   fps: 7,
   qrbox : {
@@ -5,9 +6,24 @@ let config = {
   }
 }
 
-const html5QrCode = new Html5Qrcode(
-  "reader", { formatsToSupport: [ Html5QrcodeSupportedFormats.QR_CODE ] });
-const qrCodeSuccessCallback = (decodedText, decodedResult) => {
-    /* handle success */
-};
-html5QrCode.start({ facingMode: "user" }, config, qrCodeSuccessCallback);
+// This method will trigger user permissions
+Html5Qrcode.getCameras().then(devices => {
+  /**
+   * devices would be an array of objects of type:
+   * { id: "id", label: "label" }
+   */
+  if (devices && devices.length) {
+    var cameraId = devices[0].id;
+    // .. use this to start scanning.
+  }
+}).catch(err => {
+  // handle err
+});
+
+
+//const html5QrCode = new Html5Qrcode(
+//  "reader", { formatsToSupport: [ Html5QrcodeSupportedFormats.QR_CODE ] });
+//const qrCodeSuccessCallback = (decodedText, decodedResult) => {
+//    /* handle success */
+//};
+//html5QrCode.start({ facingMode: "user" }, config, qrCodeSuccessCallback);
