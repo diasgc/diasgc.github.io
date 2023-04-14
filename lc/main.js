be031514b492943e4f70103d5e6e7ad8a9169cb0
diasgc <gustavocdias@me.com>
2023-04-14 14:38:54 +0100

    upd

--- lc/main.js
+++ lc/main.js
@@ -28,43 +28,47 @@ function applyZoom(){
         track.applyConstraints({ advanced: [{ zoom: eZoom.value * 1 }] });
 }
 
+function appendCap(table, cap, cc, onChange){
+    let rh = document.createElement('tr');
+    let h = rh.insertCell();
+    h.className = 'opt-desc'; 
+    h.innerHTML = cap;
+    rh.appendChild(h);
+    table.appendChild(rh);
+    let r = document.createElement('tr');
+    let c = r.insertCell();
+    let ds1 = document.createElement('div');
+    ds1.className = 'opt-desc';
+    ds1.innerHTML = "......";
+    let i = document.createElement('input');
+    i.id = "cam-" + cap;
+    i.type = 'range';
+    i.min = cc.min;
+    i.max = cc.max;
+    i.step = cc.step;
+    i.cap = cap;
+    i.innerText = JSON.stringify(cap);
+    i.onchange = onChange(i,ds1);
+    c.appendChild(i);
+    r.appendChild(c);
+    let d1 = r.insertCell();
+    d1.appendChild(ds1);
+    r.appendChild(d1);
+    table.appendChild(r);
+}
+
 function configScreen(caps){
     let table = document.getElementById('cam-table');
     for (var cap in caps){
         if (caps.hasOwnProperty(cap)){
             let cc = caps[cap];
             if (Object.hasOwn(cc,'min') && Object.hasOwn(cc,'max') && Object.hasOwn(cc,'step')){
-                let rh = document.createElement('tr');
-                let h = rh.insertCell();
-                h.className = 'opt-desc'; 
-                h.innerHTML = cap;
-                rh.appendChild(h);
-                table.appendChild(rh);
-                let r = document.createElement('tr');
-                let c = r.insertCell();
-                let ds1 = document.createElement('div');
-                ds1.className = 'opt-desc';
-                ds1.innerHTML = "......";
-                let i = document.createElement('input');
-                i.id = "cam-" + cap;
-                i.type = 'range';
-                i.min = cc.min;
-                i.max = cc.max;
-                i.step = cc.step;
-                i.cap = cap;
-                i.innerText = JSON.stringify(cap);
-                i.onchange = () => {
+                appendCap(table, cap, cc, (i,ds1) => {
                     var constraint = {};
                     constraint[i.cap] = i.value;
                     ds1.innerText = (i.value*1).toFixed(2);
                     track.applyConstraints({ advanced: [constraint] });
-                };
-                c.appendChild(i);
-                r.appendChild(c);
-                let d1 = r.insertCell();
-                d1.appendChild(ds1);
-                r.appendChild(d1);
-                table.appendChild(r);
+                });
             }
         }
     }
