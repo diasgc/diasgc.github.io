aae7b351bed5a2d8b8d4d8a198409e850d56890b
diasgc <gustavocdias@me.com>
2023-06-16 15:35:32 +0100

    upd

--- exif/main.js
+++ exif/main.js
@@ -1,8 +1,32 @@
 const res=document.getElementById('result');
+
+function addCard(parent, key,val){
+    let item=document.createElement('div');
+    item.className="card";
+    let k=document.createElement('p');
+    k.className="cardKey";
+    k.innerHTML=value;
+    let v=document.createElement('p');
+    v.className="cardValue";
+    item.appendChild(v);
+    item.appendChild(k);
+    parent.appendChild(item);
+}
+
+function populate(parent, obj){
+    for (const [key, value] of Object.entries(obj)) {
+        if (value instanceof Object)
+            populate(parent, value);
+        else
+            addCard(parent,key,value);
+    }
+}
+
 document.getElementById('picker').addEventListener('change', async e => {
     let file = e.target.files[0];
     EXIF.getData(file, function () {
-        var MetaData = EXIF.getAllTags(this);
-        res.innerHTML=JSON.stringify(MetaData, null, "\t")
+        let meta = EXIF.getAllTags(this);
+        populate(res,meta);
+        //res.innerHTML=JSON.stringify(meta, null, "\t")
       });
 })
