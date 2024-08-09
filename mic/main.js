2f8a1935d5a6551f28712623e1d4f5a26d40a173
diasgc <gustavocdias@me.com>
2024-08-10 00:20:58 +0100

    upd

--- mic/main.js
+++ mic/main.js
@@ -3,36 +3,28 @@ import * as THREE from 'three';
 let scene, camera, renderer, analyser, uniforms;
 
 const startButton = document.getElementById( 'startButton' );
-startButton.addEventListener( 'click', init );
+startButton.addEventListener( 'click', () => {
+    navigator.mediaDevices.getUserMedia( { audio: true, video: false } ).then(init);
+} );
 
-function init() {
-
+function init(stream){
     const fftSize = 128;
-
-    //
-
     const overlay = document.getElementById( 'overlay' );
     overlay.remove();
 
-    //
-
     const container = document.getElementById( 'container' );
-
     scene = new THREE.Scene();
-
     camera = new THREE.Camera();
 
-    navigator.mediaDevices.getUserMedia( { audio: true, video: false } ).then( stream => {
-        const listener = new THREE.AudioListener();
-        const audio = new THREE.Audio( listener );
-        const context = listener.context;
-        const source = context.createMediaStreamSource( stream );
-        audio.setNodeSource( source );
-        analyser = new THREE.AudioAnalyser( audio, fftSize );
-        uniforms = {
-            tAudioData: { value: new THREE.DataTexture( analyser.data, fftSize / 2, 1, THREE.RedFormat ) }
-        };
-    } );
+    const listener = new THREE.AudioListener();
+    const audio = new THREE.Audio( listener );
+    const context = listener.context;
+    const source = context.createMediaStreamSource( stream );
+    audio.setNodeSource( source );
+    analyser = new THREE.AudioAnalyser( audio, fftSize );
+    uniforms = {
+        tAudioData: { value: new THREE.DataTexture( analyser.data, fftSize / 2, 1, THREE.RedFormat ) }
+    };
 
     const material = new THREE.ShaderMaterial( {
 
@@ -62,17 +54,11 @@ function init() {
 }
 
 function onWindowResize() {
-
     renderer.setSize( window.innerWidth, window.innerHeight );
-
 }
 
 function animate() {
-
     analyser.getFrequencyData();
-
     uniforms.tAudioData.value.needsUpdate = true;
-
     renderer.render( scene, camera );
-
 }
