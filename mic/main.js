c2fc13aec2a410b22798231485506d99162c9c0d
diasgc <gustavocdias@me.com>
2024-08-11 23:35:23 +0100

    frag03

--- mic/main.js
+++ mic/main.js
@@ -48,7 +48,7 @@ function init(stream){
 
         uniforms: uniforms,
         vertexShader: document.getElementById( 'vertexShader' ).textContent,
-        fragmentShader: document.getElementById( 'frag01' ).textContent
+        fragmentShader: document.getElementById( 'frag03' ).textContent
 
     } );
 
