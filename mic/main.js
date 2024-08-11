7b5d5d8294f24b4e8e12d7bc85e43ed158398973
diasgc <gustavocdias@me.com>
2024-08-11 23:43:42 +0100

    upd

--- mic/main.js
+++ mic/main.js
@@ -48,7 +48,7 @@ function init(stream){
 
         uniforms: uniforms,
         vertexShader: document.getElementById( 'vertexShader' ).textContent,
-        fragmentShader: document.getElementById( 'frag03' ).textContent
+        fragmentShader: document.getElementById( 'frag02' ).textContent
 
     } );
 
