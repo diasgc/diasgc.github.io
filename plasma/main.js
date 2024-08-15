56d433e3690e1ed3307db486695e53b09642797d
diasgc <gustavocdias@me.com>
2024-08-15 21:41:51 +0100

    plasmaball

---
+++ plasma/main.js
@@ -0,0 +1,45 @@
+import * as THREE from 'three';
+
+let scene, camera, geometry, renderer, analyser, uniforms;
+
+scene = new THREE.Scene();
+// full screen
+camera = new THREE.OrthographicCamera( - 1, 1, 1, - 1, 0, 1 );
+geometry = new THREE.PlaneGeometry( 2, 2 );
+
+uniforms = {
+    opacity: { value: 1.0 },
+    iTime: { value: 0.1 }
+};
+
+const material = new THREE.ShaderMaterial( {
+
+    uniforms: uniforms,
+    vertexShader: document.getElementById( 'vertexShader' ).textContent,
+    fragmentShader: document.getElementById( 'frag02' ).textContent
+
+} );
+
+const mesh = new THREE.Mesh( geometry, material );
+scene.add( mesh );
+
+renderer = new THREE.WebGLRenderer( { antialias: true } );
+//renderer.setPixelRatio( window.devicePixelRatio );
+// Square it!
+renderer.setPixelRatio( 1 );
+renderer.setSize( window.innerWidth, window.innerHeight );
+renderer.setAnimationLoop( animate );
+document.body.appendChild(renderer.domElement);
+
+window.addEventListener( 'resize', onWindowResize );
+
+function onWindowResize() {
+    renderer.setSize( window.innerWidth, window.innerHeight );
+}
+
+function animate() {
+    analyser.getFrequencyData();
+    uniforms.tAudioData.value.needsUpdate = true;
+    uniforms.iTime.value += 0.1;
+    renderer.render( scene, camera );
+}
