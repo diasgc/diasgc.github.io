cc4932f724ac27d42014bc30fee239a093bfabe2
diasgc <gustavocdias@me.com>
2024-08-17 22:48:48 +0100

    upd

--- cpick/main.js
+++ cpick/main.js
@@ -1,16 +1,7 @@
-/*
- *  Copyright (c) 2015 The WebRTC project authors. All Rights Reserved.
- *
- *  Use of this source code is governed by a BSD-style license
- *  that can be found in the LICENSE file in the root of the source
- *  tree.
- */
-
 'use strict';
 
 // Put variables in global scope to make them available to the browser console.
 const video = document.querySelector('video');
-const canvas = window.canvas = document.querySelector('canvas');
 
 const constraints = {
   audio: false,
@@ -18,7 +9,6 @@ const constraints = {
 };
 
 function handleSuccess(stream) {
-  window.stream = stream; // make stream available to browser console
   video.srcObject = stream;
 }
 
