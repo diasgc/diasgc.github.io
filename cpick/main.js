f37a58b5ad3966f241d292fea8a4e6395f9f243b
diasgc <gustavocdias@me.com>
2024-08-17 22:01:52 +0100

    upd

--- cpick/main.js
+++ cpick/main.js
@@ -1,26 +1,38 @@
-    const currFacing = 'environment';
-    const videoPreview = document.getElementById('camera');
-    async function getDevices() {
-        const devices = await navigator.mediaDevices.enumerateDevices();
-        console.log("devices: " + devices);
-    }
-    getDevices();
-    const startCam = (facingMode = 'environment') => {
-        navigator.mediaDevices.getUserMedia({
-            video: {
-                facingMode,
-                width: {
-                    max: 1920,
-                    ideal: 1080
-                },
-                height: {
-                    max: 1080,
-                    ideal: 720
-                }
-            }
-        }).then((stream) => {
-            videoPreview.srcObject = stream;
-        })
-    }
+/*
+ *  Copyright (c) 2015 The WebRTC project authors. All Rights Reserved.
+ *
+ *  Use of this source code is governed by a BSD-style license
+ *  that can be found in the LICENSE file in the root of the source
+ *  tree.
+ */
 
-    startCam(currFacing);
+'use strict';
+
+// Put variables in global scope to make them available to the browser console.
+const video = document.querySelector('video');
+const canvas = window.canvas = document.querySelector('canvas');
+canvas.width = 480;
+canvas.height = 360;
+
+const button = document.querySelector('button');
+button.onclick = function() {
+  canvas.width = video.videoWidth;
+  canvas.height = video.videoHeight;
+  canvas.getContext('2d').drawImage(video, 0, 0, canvas.width, canvas.height);
+};
+
+const constraints = {
+  audio: false,
+  video: true
+};
+
+function handleSuccess(stream) {
+  window.stream = stream; // make stream available to browser console
+  video.srcObject = stream;
+}
+
+function handleError(error) {
+  console.log('navigator.MediaDevices.getUserMedia error: ', error.message, error.name);
+}
+
+navigator.mediaDevices.getUserMedia(constraints).then(handleSuccess).catch(handleError);
