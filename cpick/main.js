d78da2874ad442e1f849171f7a5a31669181ae9f
diasgc <gustavocdias@me.com>
2024-08-17 22:13:04 +0100

    upd

--- cpick/main.js
+++ cpick/main.js
@@ -11,15 +11,6 @@
 // Put variables in global scope to make them available to the browser console.
 const video = document.querySelector('video');
 const canvas = window.canvas = document.querySelector('canvas');
-canvas.width = 480;
-canvas.height = 360;
-
-const button = document.querySelector('button');
-button.onclick = function() {
-  canvas.width = video.videoWidth;
-  canvas.height = video.videoHeight;
-  canvas.getContext('2d').drawImage(video, 0, 0, canvas.width, canvas.height);
-};
 
 const constraints = {
   audio: false,
