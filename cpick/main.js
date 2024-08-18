94d7c5aa04ddd20be3c69ec76541b2110b8de006
diasgc <gustavocdias@me.com>
2024-08-18 18:25:01 +0100

    upd

--- cpick/main.js
+++ cpick/main.js
@@ -1,49 +1,23 @@
-var playing = false;
-var mask = new Image();
-var video = document.getElementById('video');
-var canvas = document.getElementById('canvas');
-var context = canvas.getContext('2d');
-var video = document.createElement('video');
-video.muted = true;
-video.autoplay = true;
-mask.src = "https://upload.wikimedia.org/wikipedia/commons/thumb/9/96/Male_head_silhouette.svg/720px-Male_head_silhouette.svg.png";
-// stacksnippets don't work well with gUM...
-if(navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
-    navigator.mediaDevices.getUserMedia({ video: true }).then(function(stream) {
-        video.srcObject = stream;
-        video.play();
-    });
-} else {
-  // so we'll use a video about volcanos from wikimedia
-  video.crossOrigin = mask.crossOrigin = "anonymous";    
-  // wait for the image has loaded
-  mask.onload = function() {
-    video.src = "https://upload.wikimedia.org/wikipedia/commons/transcoded/2/22/Volcano_Lava_Sample.webm/Volcano_Lava_Sample.webm.360p.webm";
-    video.play();
-  }
-}
-video.onplaying = startRendering;
-video.onpause = stopRendering;
+document.addEventListener("DOMContentLoaded", () => {
+  let but = document.getElementById("but");
+  let video = document.getElementById("vid");
+  let mediaDevices = navigator.mediaDevices;
+  vid.muted = true;
+  but.addEventListener("click", () => {
 
-function startRendering() {
-  playing = true;
-  requestAnimationFrame( render );
-}
-function stopRendering() {
-  playing = false;
-}
-function render() {
-  context.globalCompositeOperation = "copy";
-  context.drawImage(mask, 0, 0, canvas.height * mask.height/mask.width, canvas.height);
-  context.globalCompositeOperation = "source-in";
-  context.drawImage(video, 0, 0);
-  if( playing ) {
-    requestAnimationFrame( render );
-  }
-}
-
-
-document.getElementById("snap").addEventListener("click", function() {
-  var context = document.getElementById("still-canvas").getContext('2d');
-   context.drawImage(canvas, 0, 0, 640, 480);
+      // Accessing the user camera and video.
+      mediaDevices
+          .getUserMedia({
+              video: true,
+              audio: true,
+          })
+          .then((stream) => {
+              // Changing the source of video to current stream.
+              video.srcObject = stream;
+              video.addEventListener("loadedmetadata", () => {
+                  video.play();
+              });
+          })
+          .catch(alert);
+  });
 });
