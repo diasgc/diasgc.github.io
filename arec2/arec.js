25f9e8f0f73d534db4befdf202de8447eae13948
diasgc <gustavocdias@me.com>
2024-12-26 15:48:11 +0000

    upd

--- arec2/arec.js
+++ arec2/arec.js
@@ -33,12 +33,16 @@ const timer = {
 }
 
 const devices = {
-  inputDevices: '',
+  inputDevices: {},
   log: document.getElementById('logs'),
   init: function(){
     this.log.innerText = '';
-    navigator.mediaDevices
-    .enumerateDevices()
+    navigator.mediaDevices.getUserMedia({ audio: true, video: false });
+    const supportedConstraints = navigator.mediaDevices.getSupportedConstraints();
+      Object.keys(supportedConstraints).forEach((key) => {
+        console.log(`${key}: ${supportedConstraints[key]}`);
+      });
+    navigator.mediaDevices.enumerateDevices()
     .then((devices) => {
       devices.forEach((device) => {
         this.log.innerText += `${device.kind}: ${device.label} id = ${device.deviceId}\n`;
@@ -56,7 +60,7 @@ function rmic(){
   if (micCtl.micOn.checked){
     divMain.disabled = false;
     divMain.style.opacity = 1;
-    navigator.mediaDevices.getUserMedia({ audio: true });
+    //navigator.mediaDevices.getUserMedia({ audio });
     devices.init();
     startStopButton.disabled = false;
   } else {
@@ -103,21 +107,17 @@ const fsBuilder = {
 
 const inputCtl = {
   fsi: document.getElementById('fs-input'),
-  ich: { name: "channels", entries: { "mono": 1, "stereo*": 2 } },
-  ibt: { name: "bitdepth", entries: { "8": 8, "16*": 16 } },
-  isr: { name: "samplerate",
-    entries: {
-      "8k": 8000,
-      "11k": 11025,
-      "22k": 22050,
-      "44k": 44100,
-      "48k*": 48000,
-      "96k": 96000
-    }
-  },
-  iag: { name: "gain", entries: { "off": false, "on*": true } },
-  inr: { name: "noise", entries: { "off*": false, "on": true } },
-  iec: { name: "echo", entries: { "off*": false, "on": true } },
+  audioConstraints: [ 'channelCount', 'sampleSize', 'sampleRate','autoGainControl', 'echoCancellation', 'latency', 'noiseSuppression', 'pan', 'suppressLocalAudioPlayback','voiceIsolation', 'deviceId'],
+  
+  channelCount:     { name: "channels", entries: { "mono": 1, "stereo*": 2 } },
+  sampleSize:       { name: "bitsize", entries: { "8": 8, "16*": 16 } },
+  sampleRate:       { name: "samplerate", entries: {"8k": 8000, "11k": 11025, "22k": 22050, "44k": 44100, "48k*": 48000, "96k": 96000 } },
+  autoGainControl:  { name: "autogain", entries: { "off": false, "on*": true } },
+  noiseSuppression: { name: "supNoise", entries: { "off*": false, "on": true } },
+  echoCancellation: { name: "supEcho", entries: { "off*": false, "on": true } },
+  voiceIsolation:   { name: "voice", entries: { "off*": false, "on": true } },
+  suppressLocalAudioPlayback: { name: "supPlay", entries: { "off*": false, "on": true } },
+  
   options: {
     echoCancellation: false,
     noiseSuppression: false,
@@ -130,16 +130,14 @@ const inputCtl = {
   },
   init: function(){
     this.fsi.replaceChildren();
-    this.fsi.appendChild(fsBuilder.build("radio", this.ich, this.options, "channelCount"));
-    this.fsi.appendChild(fsBuilder.build("radio", this.ibt, this.options, "sampleSize"));
-    this.fsi.appendChild(fsBuilder.build("radio", this.isr, this.options, "sampleRate"));
-    this.fsi.appendChild(fsBuilder.build("radio", this.iag, this.options, "autoGainControl"));
-    this.fsi.appendChild(fsBuilder.build("radio", this.inr, this.options, "noiseSuppression"));
-    this.fsi.appendChild(fsBuilder.build("radio", this.iec, this.options, "echoCancellation"));
+    this.audioConstraints.forEach(constraint => {
+      if (this[constraint])
+        this.fsi.appendChild(fsBuilder.build("radio", this[constraint], this.options, constraint));
+    });
   },
   getOptions: function(){
     return this.options;
-  }
+  },
 }
 
 const outputCtl = {
@@ -155,7 +153,7 @@ const outputCtl = {
     audioBitsPerSecond : 256000,
     audioBitrateMode : "variable",
     mimeType: "audio/webm;codecs=opus",
-    container: 'mp4',
+    container: 'webm',
     codec: 'opus',
   },
   init: function(){
@@ -173,24 +171,6 @@ const outputCtl = {
     delete opts.codec;
     return opts;
   }
-}
-
-function saveFile(data, filename, type) {
-  const blob = new Blob([data], { type: type });
-  const a = document.createElement('a');
-  a.href = URL.createObjectURL(blob);
-  a.download = filename;
-  document.body.appendChild(a);
-  a.click();
-  document.body.removeChild(a);
-}
-
-function getTimestampFilename() {
-  return "rec-" + new Date(Date.now())
-    .toISOString()
-    .slice(0, 19)
-    .replace(/-|:/g,'')
-    .replace(/T/g,'-');
 }
 
 const micCtl = {
@@ -213,7 +193,7 @@ const dataManager = {
     const blob = new Blob(this.chunks, { type: outputCtl.mimeType });
     const a = document.createElement('a');
     a.href = URL.createObjectURL(blob);
-    a.download = this.getTimestampFilename() + "." + outputCtl.options.container;
+    a.download = this.getTimestampFilename() + "." + outputCtl.options.container.replace('mp4','m4a');
     document.body.appendChild(a);
     a.click();
     document.body.removeChild(a);
@@ -268,7 +248,7 @@ outputCtl.init();
 
 var txt = document.getElementById('log');
 const { createFFmpeg } = FFmpeg;
-const ffmpeg = createFFmpeg({ log: true, logger: ({ message }) => { txt.value += "\n" + message; } });
+const ffmpeg = createFFmpeg({ log, logger: ({ message }) => { txt.value += "\n" + message; } });
 const transcode = async ({ target: { files } }) => {
   const message = document.getElementById('message');
   const { name } = files[0];
