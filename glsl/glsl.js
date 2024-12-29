317bc580028fc24660e5532275243b99f4608904
diasgc <gustavocdias@me.com>
2024-12-29 20:36:38 +0000

    shadertoy style

--- glsl/glsl.js
+++ glsl/glsl.js
@@ -1,167 +1,114 @@
-let gl = null;
-let glCanvas = null;
-
-// Aspect ratio and coordinate system
-// details
-
-let aspectRatio;
-let currentRotation = [0, 1];
-let currentScale = [1.0, 1.0];
-
-// Vertex information
-
-let vertexArray;
-let vertexBuffer;
-let vertexNumComponents;
-let vertexCount;
-
-// Rendering data shared with the
-// scalers.
-
-let uScalingFactor;
-let uGlobalColor;
-let uRotationVector;
-let aVertexPosition;
-
-// Animation timing
-
-let shaderProgram;
-let currentAngle;
-let previousTime = 0.0;
-let degreesPerSecond = 90.0;
-
 class GlCanvas {
 
   constructor(id, vertexId, fragmentId){
+    
     this.glCanvas = id ? document.getElementById(id) : document.createElement('canvas');
-    this.gl = glCanvas.getContext("webgl");
-    this.shaderSet = [
-        { type: gl.VERTEX_SHADER,   id: vertexId },
-        { type: gl.FRAGMENT_SHADER, id: fragmentId },
-      ];
-    }
+    this.gl = this.glCanvas.getContext("webgl");
+    this.bufObj = {};
+    this.mousepos = [0,0];
+    this.loadProgram(vertexId, fragmentId, program => this.init(program));
   }
+
+  init(program){
+    const gl = this.gl;
+    program.position = gl.getAttribLocation(program, "position");
+    program.iTime = gl.getUniformLocation(program, "iTime");
+    program.iMouse = gl.getUniformLocation(program, "iMouse");
+    program.iResolution = gl.getUniformLocation(program, "iResolution");
+    gl.useProgram(program);
+
+    var pos = [ -1, -1, 1, -1, 1, 1, -1, 1 ];
+    var inx = [ 0, 1, 2, 0, 2, 3 ];
+    
+    let bufObj = {};
+    bufObj.pos = gl.createBuffer();
+    gl.bindBuffer( gl.ARRAY_BUFFER, bufObj.pos );
+    gl.bufferData( gl.ARRAY_BUFFER, new Float32Array( pos ), gl.STATIC_DRAW );
+    bufObj.inx = gl.createBuffer();
+    bufObj.inx.len = inx.length;
+    gl.bindBuffer( gl.ELEMENT_ARRAY_BUFFER, bufObj.inx );
+    gl.bufferData( gl.ELEMENT_ARRAY_BUFFER, new Uint16Array( inx ), gl.STATIC_DRAW );
+    gl.enableVertexAttribArray( program.position );
+    gl.vertexAttribPointer( program.position, 2, gl.FLOAT, false, 0, 0 ); 
+    
+    gl.enable( gl.DEPTH_TEST );
+    gl.clearColor( 0.0, 0.0, 0.0, 1.0 );
+
+    window.onresize = this.resize;
+    this.resize();
+    this.gl = gl;
+    this.program = program;
+    this.bufObj = bufObj;
+  }
+
+  start(){
+    this.startTime = Date.now();
+    this.render();
+  }
+
+  render(){
+    const gl = this.gl;
+    const glCanvas = this.glCanvas;
+    const program = this.program;
+    const bufObj = this.bufObj;
+    const startTime = this.startTime;
+    const mousepos = [0,0];
+    
+    function frame(){
+      gl.viewport( 0, 0, glCanvas.width, glCanvas.height );
+      gl.clear( gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT );
+     
+      gl.uniform1f(program.iTime, (Date.now() - startTime)/1000.0);
+      gl.uniform2f(program.iResolution, glCanvas.width, glCanvas.height);
+      gl.uniform2f(program.iMouse, mousepos[0], mousepos[1]);
+      gl.drawElements( gl.TRIANGLES, bufObj.inx.len, gl.UNSIGNED_SHORT, 0 );
+      requestAnimationFrame(frame);
+    }
+
+    window.requestAnimationFrame(frame);
+  }
+
+  resize(){
+    this.glCanvas.width = window.innerWidth;
+    this.glCanvas.height = window.innerHeight;
+  }
+
+  loadProgram(vertexId, fragmentId, callback){
+    const program = this.gl.createProgram();
+    if (this.compileShader(vertexId, this.gl.VERTEX_SHADER,
+        shader => this.gl.attachShader(program, shader))
+      && this.compileShader(fragmentId, this.gl.FRAGMENT_SHADER,
+        shader => this.gl.attachShader(program, shader))){
+          this.gl.linkProgram(program);
+          if (this.gl.getProgramParameter(program, this.gl.LINK_STATUS)) {
+            callback(program);
+            return true;  
+          }
+          console.log(`Error linking program: ${this.gl.getProgramInfoLog(program)}`);
+      };
+    return false;
+  }
+
+  compileShader(id, type, callback){
+    const code = document.getElementById(id).firstChild.nodeValue;
+    const shader = this.gl.createShader(type);
+  
+    this.gl.shaderSource(shader, code);
+    this.gl.compileShader(shader);
+  
+    if (this.gl.getShaderParameter(shader, this.gl.COMPILE_STATUS)) {
+      callback(shader);
+      return true;
+    }
+    console.log(`error compiling ${id}: ${this.gl.getShaderInfoLog(shader)}`);
+    return false;
+  }
+}
 
 window.addEventListener("load", startup, false);
 
+
 function startup() {
-  glCanvas = document.getElementById("gl-canvas");
-  gl = glCanvas.getContext("webgl");
-
-  const shaderSet = [
-    {
-      type: gl.VERTEX_SHADER,
-      id: "vertexShader",
-    },
-    { 
-      type: gl.FRAGMENT_SHADER,
-      id: "fragmentShader",
-    },
-  ];
-
-  shaderProgram = buildShaderProgram(shaderSet);
-
-  aspectRatio = glCanvas.width / glCanvas.height;
-  currentRotation = [0, 1];
-  currentScale = [1.0, aspectRatio];
-
-  vertexArray = new Float32Array([
-    -0.5, 0.5, 0.5, 0.5, 0.5, -0.5, -0.5, 0.5, 0.5, -0.5, -0.5, -0.5,
-  ]);
-
-  vertexBuffer = gl.createBuffer();
-  gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
-  gl.bufferData(gl.ARRAY_BUFFER, vertexArray, gl.STATIC_DRAW);
-
-  vertexNumComponents = 2;
-  vertexCount = vertexArray.length / vertexNumComponents;
-
-  currentAngle = 0.0;
-
-  animateScene();
-}
-
-function buildShaderProgram(shaderInfo) {
-  const program = gl.createProgram();
-
-  shaderInfo.forEach((desc) => {
-    const shader = compileShader(desc.id, desc.type);
-
-    if (shader) {
-      gl.attachShader(program, shader);
-    }
-  });
-
-  gl.linkProgram(program);
-
-  if (!gl.getProgramParameter(program, gl.LINK_STATUS)) {
-    console.log("Error linking shader program:");
-    console.log(gl.getProgramInfoLog(program));
-  }
-
-  return program;
-}
-
-function compileShader(id, type) {
-  const code = document.getElementById(id).firstChild.nodeValue;
-  const shader = gl.createShader(type);
-
-  gl.shaderSource(shader, code);
-  gl.compileShader(shader);
-
-  if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
-    console.log(
-      `Error compiling ${
-        type === gl.VERTEX_SHADER ? "vertex" : "fragment"
-      } shader:`,
-    );
-    console.log(gl.getShaderInfoLog(shader));
-  }
-  return shader;
-}
-
-function animateScene() {
-  gl.viewport(0, 0, glCanvas.width, glCanvas.height);
-  gl.clearColor(0.8, 0.9, 1.0, 1.0);
-  gl.clear(gl.COLOR_BUFFER_BIT);
-
-  const radians = (currentAngle * Math.PI) / 180.0;
-  currentRotation[0] = Math.sin(radians);
-  currentRotation[1] = Math.cos(radians);
-
-  gl.useProgram(shaderProgram);
-
-  uScalingFactor = gl.getUniformLocation(shaderProgram, "uScalingFactor");
-  uGlobalColor = gl.getUniformLocation(shaderProgram, "uGlobalColor");
-  uRotationVector = gl.getUniformLocation(shaderProgram, "uRotationVector");
-
-  gl.uniform2fv(uScalingFactor, currentScale);
-  gl.uniform2fv(uRotationVector, currentRotation);
-  gl.uniform4fv(uGlobalColor, [0.1, 0.7, 0.2, 1.0]);
-
-  gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
-
-  aVertexPosition = gl.getAttribLocation(shaderProgram, "aVertexPosition");
-
-  gl.enableVertexAttribArray(aVertexPosition);
-  gl.vertexAttribPointer(
-    aVertexPosition,
-    vertexNumComponents,
-    gl.FLOAT,
-    false,
-    0,
-    0,
-  );
-
-  gl.drawArrays(gl.TRIANGLES, 0, vertexCount);
-
-  requestAnimationFrame((currentTime) => {
-    const deltaAngle =
-      ((currentTime - previousTime) / 1000.0) * degreesPerSecond;
-
-    currentAngle = (currentAngle + deltaAngle) % 360;
-
-    previousTime = currentTime;
-    animateScene();
-  });
+  const webGl = new GlCanvas('gl-canvas', 'vertexShader', 'fragmentShader');
+  webGl.start();
 }
