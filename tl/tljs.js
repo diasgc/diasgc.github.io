8fb33c5688349c1aff0696cfabe7634befe213c3
diasgc <gustavocdias@me.com>
2025-01-05 19:24:26 +0000

    upd

--- tl/tljs.js
+++ tl/tljs.js
@@ -7,13 +7,63 @@ if ("serviceWorker" in navigator) {
   })
 }
 
+const videoOpts = {
+  width: screen.width,
+  height: screen.height,
+  facingMode: {ideal:"environment"},
+}
+
+/*
+aspectRatio
+autoGainControl
+brightness
+channelCount
+colorTemperature
+contrast
+deviceId
+displaySurface
+echoCancellation
+exposureCompensation
+exposureMode
+exposureTime
+facingMode
+focusDistance
+focusMode
+frameRate
+groupId
+height
+iso
+latency
+noiseSuppression
+pan
+pointsOfInterest
+resizeMode
+sampleRate
+sampleSize
+saturation
+sharpness
+suppressLocalAudioPlayback
+tilt
+torch
+voiceIsolation
+whiteBalanceMode
+width
+zoom
+*/
+
 const video = document.getElementById('tl-video');
+const log = document.getElementById('log');
+
 navigator.mediaDevices
-  .getUserMedia({ video: true, audio: false })
-  .then((stream) => {
-    video.srcObject = stream;
-    video.play();
-  })
+  .getUserMedia({ video: videoOpts, audio: false })
+  .then((stream) => init(stream))
   .catch((err) => {
     console.error(`An error occurred: ${err}`);
   });
+
+function init(stream) {
+  video.srcObject = stream;
+  video.play();
+  const track = stream.getVideoTracks()[0];
+  log.innerHTML = JSON.stringify(track.getCapabilities(), null, 2);
+}
