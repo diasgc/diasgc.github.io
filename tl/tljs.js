d84588c49160d3fc5266a054eee6717dd02fdeee
diasgc <gustavocdias@me.com>
2025-01-05 18:58:37 +0000

    tl basic

--- tl/tljs.js
+++ tl/tljs.js
@@ -6,3 +6,14 @@ if ("serviceWorker" in navigator) {
       .catch(err => console.log("service worker not registered", err))
   })
 }
+
+const video = document.getElementById('tl-video');
+navigator.mediaDevices
+  .getUserMedia({ video: true, audio: false })
+  .then((stream) => {
+    video.srcObject = stream;
+    video.play();
+  })
+  .catch((err) => {
+    console.error(`An error occurred: ${err}`);
+  });
