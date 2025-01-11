75ae0af935832dad7c554768455900025b8319c4
diasgc <gustavocdias@me.com>
2025-01-11 20:02:12 +0000

    widget ruler

---
+++ widgets/ruler.js
@@ -0,0 +1,66 @@
+function createRuler(container, callback, options){
+
+  options = options || {};
+  let opts = {
+    min: options.min || 0,
+    max: options.max || 100,
+    tickSpacing: options.tickSpacing || 5,
+    lineWidth: options.lineWidth || 0.5,
+    topMargin: options.topMargin || 5,
+    scaleUnits: options.scaleUnits || 10,
+    tickHeightArray: options.tickHeightArray || [20,18,14]
+  }
+
+  const divRuler = document.createElement('div');
+  divRuler.style.position = 'absolute';
+  divRuler.style.width = '100dvw';
+  divRuler.style.overflowX = 'auto';
+  container.appendChild(divRuler);
+  
+  opts.w = opts.max - opts.min;
+  const canvas = document.createElement('canvas');
+  const width = opts.w * opts.tickSpacing;
+  canvas.width = width;
+  canvas.height = divRuler.clientHeight || 50;
+  canvas.style.padding = '0 50dvw';
+
+  divRuler.onscroll = (e) => callback(opts.min + opts.w * e.currentTarget.scrollLeft / width);
+  
+  
+  const tickHeight = Array(opts.scaleUnits).fill(opts.tickHeightArray[opts.tickHeightArray.length - 1]);
+  tickHeight[0] = opts.tickHeightArray[0];
+  tickHeight[opts.scaleUnits/2] = opts.tickHeightArray[1];
+
+  const ctx = canvas.getContext('2d');
+  ctx.lineWidth = opts.lineWidth;
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  ctx.beginPath();
+
+  //base line
+  ctx.moveTo(0, opts.topMargin);
+  ctx.lineTo(width, opts.topMargin);
+  ctx.stroke();
+  
+  var j = Math.round(opts.min);
+  let dmax = opts.max.toString().length * opts.tickSpacing;
+  ctx.fillText(opts.min, 2, tickHeight[0] + 15);
+  for (let i = 0; i <= width; i += opts.tickSpacing) {
+    let th = tickHeight[Math.abs(j % 10)];
+    ctx.moveTo(i, opts.topMargin);
+    ctx.lineTo(i, opts.topMargin + th);
+    ctx.stroke();
+    if (th === tickHeight[0])
+      ctx.fillText(j, i - (j === opts.max ? dmax : 5), th + 15);
+    j++;
+  }
+  divRuler.appendChild(canvas);
+  let m = document.createElement('div');
+  m.style.position = 'absolute';
+  m.style.width = '1px';
+  m.style.height = '50px';
+  m.style.borderLeft = '1px solid #b00';
+  m.style.left = '50%';
+  m.style.top = canvas.style.top;
+  container.appendChild(m);
+  callback(opts.min);
+}
