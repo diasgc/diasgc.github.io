7930bc800088d9b00f464a1d79e8819bd1adb8bf
diasgc <gustavocdias@me.com>
2025-02-09 12:00:47 +0000

    test demo

--- wwp/shaders/rayleigh-mie.js
+++ wwp/shaders/rayleigh-mie.js
@@ -1,4 +1,4 @@
-const live = false;
+const live = true;
 const frag=`#pragma optimize(on)
 #pragma debug(off)
 
@@ -69,42 +69,38 @@ const float pi14 = 1.0 / (4.0 * pi);
 const float arcsec2rad = pi / 648000.0;
 const float rad2deg = 180.0 / pi;
 
+const vec3 zenDir = vec3 ( 0.0, 1.0, 0.0 );
+const vec3 cameraPos = vec3( 1.0, 0.0, 1.0 );
+
+
+const float moonFade = 2.0;
+const vec3  sunLighColor = vec3(1.0);
+const vec3  nightColor = vec3(0.04, 0.034, 0.09) * 0.32;
+
+// inverse of sun intensity steepness: (def: 0.66 = 1./1.5)
+const float sunIStep = 0.66;
+const float sunImax = 1000.0;
+const float sunImin = 300.0;
+
 // earth shadow hack, nautical twilight dark at -12º (def: pi/1.95)
-const float kCutoffAngle = pi / 1.766;
-
-// inverse of sun intensity stepness: (def: 0.66 = 1./1.5)
-const float kSunIStep = 0.66;
-const float kSunI = 1000.0;
-
-// Sun fade max (1.0)
-const float kSunMax = 1.0;
-
-// Sun fade factor (0.2) lower values increase contrast
-const float kSunFade = 0.2;
+const float sunCutoffAngle = pi / 1.95; //1.766
 
 // 66 arc seconds -> degrees, and the cosine of that
 const float kSunArc = 0.999956676; //cos( arcsec2rad * 3840. );
 const float kSunDim = 2e-5;
 
-// a = angle, r = refraction
-float sunIntensity(float a, float r, float nebulosity) {
-  return mix(kSunI, 250.0, nebulosity) * (1. - exp( -kSunIStep * ( kCutoffAngle - acos(clamp(a,-1.,1.)) + r ) ));
+// Sun extinction power def 0.5
+const vec3  sunExtinctPow = vec3( 0.5 );
+
+float sunIntensity(float angle, float refraction, float cloudiness) {
+  return mix(sunImax, sunImin, cloudiness) * (1. - exp( -sunIStep * ( sunCutoffAngle - acos(clamp(angle,-1.,1.)) + refraction ) ));
 }
 
-const vec3 zenDir = vec3 ( 0.0, 1.5, 0.0 );
-vec3 cameraPos = vec3( 1.0, 0.0, 1.5 );
 
 // constants for atmospheric scattering
 // optical length at zenith for molecules
 const float zenithR = 8400.0;
 const float zenithM = 1250.0;
-
-// Sun extinction power def 0.5
-const vec3  kSunExPow = vec3( 0.5 );
-
-const float kMoonFade = 2.0;
-const vec3  kColorSun = vec3(1.0);
-const vec3  kColorNight = vec3(0.04, 0.034, 0.09) * 0.32;
 
 #define rayleighPhase(a)    pi316 * ( 1.0 + a * a )
 #define hgPhase( a, g, g2 ) pi14 * (( 1.0 - g2 ) / pow( 1.0 - 2.0 * g * a + g2, 1.5 ))
@@ -123,36 +119,37 @@ const vec3 LAMBDA = vec3( 650E-9, 550E-9, 450E-9 );
  #endif 
 
 #ifdef fastRefractiveIndex
-#define airRefractiveIndex(Tk, Pmbar, rHum) 1.0003
+#define airRefractiveIndex(kelvin, Pmbar, rHum) 1.0003
 #else
-
 // Appendix B:  Simple Shop-floor Formula for Refractive Index of Air
 // https://emtoolbox.nist.gov/Wavelength/Documentation.asp#AppendixB
-#define airRefractiveIndex(Tk, Pmbar, relH) 1.0 + 7.86e-5 * Pmbar / Tk - 1.5e-11 * relH * (pow(Tk - 273.15, 2.0) + 160.0)
-#endif
-
-#ifdef fastMolsPerVolume
-#define molsPerVolume(P,Tk) 2.545E25
-#else
-// Boltzmann constant
-const float KB = 1.3806488e-23;
-#define molsPerVolume(P,Tk) 100.0 * P / (KB * Tk)
-#endif
-
-#if 0
-float airRefractiveIndex(float Tk, float P, float H){
-  float t = Tk - 273.15;
-  float e = H * ( 1. + 1e-4 * (7.2 + P * (0.00320 + 5.9e-7 * t * t))) * 6.1121 * exp( ( 18.678 - t / 234.5 ) * t / (t + 257.14));
-  float Nr = (77.6 * P - e * (5.6  + 3.75e5 / Tk)) / Tk;
+// 1. + 7.86e-4 * kPa / kelvin - 1.5e-11 * relH * (kelvin - 273.15)² + 160.0 (1kPa = 10mbar)
+float airRefractiveIndex(float kelvin, float Pmbar, float relH) {
+  return 1.0 + 7.86e-5 * Pmbar / kelvin - 1.5e-11 * relH * (pow(kelvin - 273.15, 2.0) + 160.0);
+}
+// full calculation
+float airRefractiveIndexFull(float kelvin, float P, float rH){
+  float t = kelvin - 273.15;
+  float e = rH * ( 1. + 1e-4 * (7.2 + P * (0.00320 + 5.9e-7 * t * t))) * 6.1121 * exp( ( 18.678 - t / 234.5 ) * t / (t + 257.14));
+  float Nr = (77.6 * P - e * (5.6  + 3.75e5 / kelvin)) / kelvin;
   // refractive index of air (default: 1.0003):
   return 1.0 + Nr * 1e-6 * exp( -altitude / zenithR );
 }
 #endif
 
+#ifdef fastMolsPerVolume
+#define molsPerVolume(Pmbar,kelvin) 2.545E25
+#else
+// Boltzmann constant
+const float KB = 1.3806488e-23;
+#define molsPerVolume(Pmbar,kelvin) 100.0 * Pmbar / (KB * kelvin)
+#endif
+
+
 //Rayleigh scattering for small particles
 #ifdef fastRayleigh
 // precalculated values - default: vec3 (6.869069761642851E-6, 1.3399870327319272E-5, 3.2714527166306815E-5)
-#define getBetaRayleigh(r, Tk, P, H ) vec3( 5.804542996261093E-6, 1.3562911419845635E-5, 3.0265902468824876E-5 )
+#define getBetaRayleigh(r, Tk, Pmbar, H ) vec3( 5.804542996261093E-6, 1.3562911419845635E-5, 3.0265902468824876E-5 )
 #else
 // depolarization factor (default: 0.0035) 0.0279
 const float def_pn = 0.0279;
@@ -166,6 +163,9 @@ vec3 getBetaRayleigh( float rayleigh, float Tk, float P, float H ){
   return pi383 * n * def_kpn / ( N * L4);
 }
 #endif
+
+
+
 
 // Mountains (https://www.shadertoy.com/view/fsdGWf)
 
@@ -204,17 +204,10 @@ vec3 H33(vec3 p) {
     return fract((p + p.yzx) * 43758.5453123);
 }
 
-vec3 hash(vec3 p) {
-    p = fract(p * vec3(127.1, 311.7, 74.7));
-    p += dot(p, p.yzx + 19.19);
-    return fract(sin(p) * 43758.5453123);
-}
-
 float N13(vec3 p) {
     vec3 i = floor(p);
     vec3 f = fract(p);
     vec3 u = f * f * (3.0 - 2.0 * f);
-    
     return mix(
         mix(
             mix(dot(H33(i + vec3(0.0, 0.0, 0.0)), f - vec3(0.0, 0.0, 0.0)),
@@ -249,9 +242,14 @@ vec3 starfield(vec2 uv, float sunpos, float clds){
 
 // Starfield#2 (https://www.shadertoy.com/view/fsSfD3) (why AI 4s23Rt)
 
-#define R12(co,s) fract(tan(distance(co * 1.61803398875, co) * s) * co.x)
+const float phi = 1.61803398874989484820459;
+float R121(vec2 co, float s){
+  return fract(tan(distance(co * phi, co) * s) * co.x);
+}
 
-#define H12(s) vec2(R12(vec2(243.234, 63.834), s) - 0.5, R12(vec2(53.1434, 13.1234), s) - 0.5)
+vec2 H21(float s){
+  return vec2( R121(vec2(243.234, 63.834), s) - 0.5, R121(vec2(53.1434, 13.1234), s) - 0.5);
+}
 
 float LS2(vec2 uv, vec2 ofs, float b, float l) {
   float len = length(uv - ofs);
@@ -263,7 +261,7 @@ vec3 sf2(vec2 uv, float sunpos, float clds) {
     return vec3(0.);
   vec3 col = vec3(0.0);
   for (float i = 0.0; i < 50.0; i++) {
-    vec2 ofs = H12(i + 1.0) * vec2(1.8, 1.1);
+    vec2 ofs = H21(i + 1.0) * vec2(1.8, 1.1);
     float r = (mod(i, 10.0) == 0.0) ? 0.5 + abs(sin(i / 50.0)) : 0.25;
     float l = 1.0 + 0.02 * (sin(fract(iTime) * 0.5 * i) + 1.0);
     col += vec3(LS2(0.5 - uv, ofs, r, l));
@@ -279,30 +277,24 @@ vec3 sf2(vec2 uv, float sunpos, float clds) {
 #define CLOUD_SMOOTH 0.23
 #define CLOUD_SPEED 0.001
 
-float N21(vec2 p) {
-    return fract(sin(p.x*100.+p.y*7446.)*8345.);
+float H12(vec2 p) {
+    return fract(sin(p.x * 100. + p.y * 7446.) * 8345.);
 }
 
-float SS(vec2 uv) {
+float N12(vec2 uv) {
     vec2 lv = fract(uv);
-    lv = lv * lv * (3.0 - 2.0 * lv);
     vec2 id = floor(uv);
-    
-    float bl = N21(id);
-    float br = N21(id + vec2(1., 0.));
-    float b = mix(bl, br, lv.x);
-
-    float tl = N21(id + vec2(0., 1.));
-    float tr = N21(id + vec2(1., 1.));
-    float t = mix(tl, tr, lv.x);
-
-    return mix(b, t, lv.y);
+    lv = lv * lv * (3.0 - 2.0 * lv);
+    return mix(
+      mix(H12(id), H12(id + vec2(1., 0.)), lv.x),
+      mix(H12(id + vec2(0., 1.)), H12(id + vec2(1., 1.)), lv.x),
+      lv.y);
 }
   
 vec3 renderClouds(vec2 uv, float sunpos, float humidity, float clouds){
   float c = 0.;
   for(float i = 1.; i < CLOUD_STEPS; i+=1.) {
-    c += SS(-iTime * CLOUD_SPEED + uv * pow(1.0 + (uv.y + humidity), i + .7 * clouds)) * pow(CLOUD_SMOOTH, i);
+    c += N12(-iTime * CLOUD_SPEED + uv * pow(1.0 + (uv.y + humidity), i + .7 * clouds)) * pow(CLOUD_SMOOTH, i);
   }
   return vec3( c * clouds * CLOUD_INTENSITY * mix(0.25, 1.5, clip(sunpos)));
 }
@@ -370,7 +362,7 @@ void mainImage( out vec4 fragColor, in vec2 fragCoord ){
   vec3 B = pow( sunEx * betaTotal * Fex, vec3( 0.5) );
   vec3 L0 = 0.5 * Fex;
   
-  vec3 night = kColorNight * (1.0 + moon * kMoonFade);
+  vec3 night = nightColor * (1.0 + moon * moonFade);
 
   
   if (vHum.z < 0.01) {
@@ -386,7 +378,7 @@ void mainImage( out vec4 fragColor, in vec2 fragCoord ){
   }
   
   // the horizon line
-  L *= mix( kColorSun, B , clip(pow(1.0 - cosGamma, 5.0)));
+  L *= mix( sunLighColor, B , clip(pow(1.0 - cosGamma, 5.0)));
   float sk = 1.2 / ( 1.2 + 1.2 * sunFd );
   float k = 0.04;
   
