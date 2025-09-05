adc48c3edd4a79345cc7b15872334e14b097692b
diasgc <gustavocdias@me.com>
2025-09-05 12:42:02 +0100

    upd

--- ubuntu-setup/sw/main.js
+++ ubuntu-setup/sw/main.js
@@ -1,7 +1,5 @@
 
 
-const main = document.getElementById('main');
-
 const utils = {
   addOption: function(parent, val, txt, selected=false){
     parent.appendChild(this.createOption(val, txt, selected));
@@ -21,6 +19,11 @@ const utils = {
     a.download = filename;
     a.click();
     URL.revokeObjectURL(url);
+  },
+  getIntProperty: function(key){
+    key = "--" + key.replace('--','');
+    const v = getComputedStyle(document.documentElement).getPropertyValue(key).trim();
+    return parseInt(v);
   },
   fetchText: async function(url){
     const res = await fetch(url);
@@ -45,7 +48,7 @@ const dmain = {
         pwa: pwa.id
       }
     });
-    this.size = parseInt(getComputedStyle(document.documentElement).getPropertyValue("--grid-max").trim());
+    this.size = utils.getIntProperty('grid-max');
     Object.keys(this.data).forEach( group => dmain.addGroup(group));
   },
   addGroup: function(group){
@@ -73,8 +76,6 @@ const dmain = {
   }
 }
 
-dmain.load();
-
 const llvm_sh = {
   url: 'https://apt.llvm.org/llvm.sh',
   id: document.getElementById('llvm'),
@@ -90,4 +91,57 @@ const llvm_sh = {
   }
 }
 
-llvm_sh.load();
+const exec = {
+  id: document.getElementById('exec'),
+  scr: '',
+  ppa: [],
+  apt: [],
+  deb: [],
+  flathub: [],
+  snap: [],
+  pwa: [],
+  script: [],
+  init: function(){
+    this.id.addEventListener('click', this.gen);
+    dmain.load();
+    llvm_sh.load();
+  },
+  gen: function(){
+    exec.scr = '#!/bin/bash';
+    dmain.id.querySelectorAll('select').forEach(s => {
+      if (s.value){
+        Array.from(s.selectedOptions).map(({ value }) => {
+          const v1 = JSON.parse(value);
+          console.log(`${s.id}: ${value}`);
+          if (exec[v1.type]){
+            exec[v1.type].push(v1.args);
+            if (v1.ppa)
+              exec.ppa.push(v1.ppa);
+          } else
+            console.log(`Unknown type ${v1.type}`);
+        });
+      }
+    });
+    if (exec.ppa.length > 0)
+      exec.scr +=`\nsudo add-apt-repository ${exec.ppa.join(' ')} -y && sudo apt update`;
+    if (exec.apt.length > 0)
+      exec.scr +=`\nsudo apt install ${exec.apt.join(' ')} -y`;
+    if (exec.snap.length > 0)
+      exec.scr +=`\nsudo snap install ${exec.snap.join(' ')}`;
+    if (exec.deb.length > 0)
+      exec.deb.forEach(deb => {
+        exec.scr += `\nname=$(basename ${deb})\necho "Downloading $name, please wait..."\nwget -q "${deb}"\nsudo dpkg -i $name && rm $name`;
+      })
+    if (exec.script.length > 0)
+      exec.scr +=`\nsudo script install ${exec.script.join(' ')}`;
+    if (exec.pwa.length > 0)
+      exec.scr +=`\nsudo pwa install ${exec.pwa.join(' ')}`;
+    const blob = new Blob([exec.scr], { type: "text/plain" });
+    utils.downloadBlob(blob,"install.sh");
+  },
+  genDeb: function(){
+
+  }
+}
+
+exec.init();
--- zc/main.js
+++ zc/main.js
@@ -3,3 +3,6 @@ setInterval(()=>{
     const d = new Date();
     t.innerText = d.toTimeString().split(' ')[0];
 },1000);
+
+//https://developers.sefaria.org/reference/get-v3-texts
+//https://www.hebcal.com/home/1663/zmanim-halachic-times-api
