e7a465c09bb40d0b9cb444b3a842ea6fc1bc52ea
diasgc <gustavocdias@me.com>
2025-09-05 18:16:58 +0100

    ubuntu: gen script

--- ubuntu-setup/sw/main.js
+++ ubuntu-setup/sw/main.js
@@ -115,6 +115,8 @@ const exec = {
           console.log(`${s.id}: ${value}`);
           if (exec[v1.type]){
             exec[v1.type].push(v1.args);
+            if (v1.pkg)
+              exec.apt.push(v1.pkg);
             if (v1.ppa)
               exec.ppa.push(v1.ppa);
           } else
@@ -124,16 +126,18 @@ const exec = {
     });
     if (exec.ppa.length > 0)
       exec.scr +=`\nsudo add-apt-repository ${exec.ppa.join(' ')} -y && sudo apt update`;
+    if (exec.deb.length > 0)
+      exec.deb.forEach(deb => {
+        exec.scr += `\nname=$(basename ${deb})\necho "Downloading $name, please wait..."\nwget -q "${deb}"\nsudo dpkg -i $name && rm $name`;
+      });
     if (exec.apt.length > 0)
       exec.scr +=`\nsudo apt install ${exec.apt.join(' ')} -y`;
     if (exec.snap.length > 0)
       exec.scr +=`\nsudo snap install ${exec.snap.join(' ')}`;
-    if (exec.deb.length > 0)
-      exec.deb.forEach(deb => {
-        exec.scr += `\nname=$(basename ${deb})\necho "Downloading $name, please wait..."\nwget -q "${deb}"\nsudo dpkg -i $name && rm $name`;
-      })
     if (exec.script.length > 0)
-      exec.scr +=`\nsudo script install ${exec.script.join(' ')}`;
+      exec.script.forEach(async script => {
+        await utils.fetchText(`scripts/${script}.sh`).then(res => exec.scr +=`\n${res}\n`);
+      });
     if (exec.pwa.length > 0)
       exec.scr +=`\nsudo pwa install ${exec.pwa.join(' ')}`;
     const blob = new Blob([exec.scr], { type: "text/plain" });
