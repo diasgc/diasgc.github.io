5a84ccc528a9062cc5eb0217216a4da957e0a8e6
diasgc <gustavocdias@me.com>
2025-09-29 23:08:49 +0100

    upd

--- zc/main.js
+++ zc/main.js
@@ -1,5 +1,6 @@
 function rotate(d){
-  const dd = d.getTime()/1000 % 86400 / 86400 * 360;
+  //const dd = d.getTime()/1000 % 86400 / 86400 * 360;
+  const dd = (d.getHours()*60 + d.getMinutes()) / 1440 * 360;
   img.style.transform = `rotate(${-dd}deg)`;
 }
 
@@ -11,7 +12,7 @@ setInterval(()=>{
     const t = document.getElementById('time');
     const d = new Date();
     rotate(d);
-    t.innerText = d.toLocaleTimeString('en-GB', { hour: '2-digit', minute: '2-digit' });;
+    t.innerText = `${d.getHours()}:${d.getMinutes()}`;
 },5000);
 
 //https://developers.sefaria.org/reference/get-v3-texts
