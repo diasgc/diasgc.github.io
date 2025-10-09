51a207001fef53902181f427e6e58e4690b12f03
diasgc <gustavocdias@me.com>
2025-10-09 22:10:47 +0100

    upd

--- zc/tnk/main.js
+++ zc/tnk/main.js
@@ -1,3 +1,43 @@
+const tnk = {
+  sefer: 0,
+  perek: 1,
+  pasuk: 1,
+  book: 'Bereshit',
+  seferim: ["Bereshit", "Shemot", "Vayikra", "Bamidbar", "Devarim"],
+  psukim: [
+    [ // Bereshit
+      [31], [25], [24], [26], [32], [22], [24], [22], [29], [32],
+      [32], [20], [18], [24], [21], [16], [27], [33], [38], [34],
+      [18], [34], [20], [67], [34], [35], [46], [22], [35], [43],
+      [55], [32], [20], [31], [29], [43], [36], [30], [23], [23],
+      [57], [38], [34], [34], [28], [34], [31], [22], [26], [26]
+    ],
+    [ // Shemot
+      [22], [25], [22], [31], [23], [30], [25], [32], [35], [29],
+      [10], [51], [16], [31], [27], [36], [16], [27], [25], [26],
+      [36], [31], [33], [18], [40], [37], [21], [43], [46], [38],
+      [18], [35], [23], [35], [35], [38], [24], [31], [43], [38]
+    ],
+    [ // Vayikra
+      [17], [16], [17], [35], [26], [23], [38], [36], [24], [20],
+      [47], [8], [59], [57], [33], [34], [16], [30], [37], [27],
+      [24], [33], [44], [23], [55], [46], [34]
+    ],
+    [ // Bamidbar
+      [54], [34], [51], [49], [31], [27], [89], [26], [23], [36],
+      [35], [16], [33], [45], [41], [50], [13], [32], [22], [29],
+      [35], [41], [30], [25], [18], [65], [23], [31], [39], [17],
+      [54], [42], [56], [29], [34], [13]
+    ],
+    [ // Devarim
+      [46], [37], [29], [49], [33], [25], [26], [20], [29], [22],
+      [32], [32], [18], [29], [23], [22], [20], [22], [21], [20],
+      [23], [30], [25], [22], [19], [19], [26], [69], [28], [20],
+      [30], [52], [29], [12]
+    ]
+  ]
+}
+
 const options = {
   date: new Date(),
   latitude: 32.0853,
@@ -16,24 +56,84 @@ navigator.geolocation.getCurrentPosition(position => {
   options.locationName = 'Current Location';
 });
 
-const i = document.getElementById('verse');
-i.addEventListener('change',()=> fetchData(i.value));
-fetchData(i.value);
+document.getElementById('pasuk-prev').addEventListener('click',() => {
+  prev();
+  const i = document.getElementById('verse');
+  i.value = `${tnk.book} ${tnk.perek}.${tnk.pasuk}`;
+  upd(i.value);
+});
 
-function fetchData(ref){
-  const url = `https://www.sefaria.org/api/v3/texts/${ref}?version=hebrew`;
+document.getElementById('pasuk-next').addEventListener('click',() => {
+  next();
+  const i = document.getElementById('verse');
+  i.value = `${tnk.book} ${tnk.perek}.${tnk.pasuk}`;
+  upd(i.value);
+});
+
+function prev(){
+  if (tnk.pasuk > 1){
+    tnk.pasuk--;
+  } else {
+    if (tnk.perek > 1){
+      tnk.perek--;
+      tnk.pasuk = tnk.psukim[tnk.sefer][tnk.perek-1][0];
+    } else {
+      if (tnk.sefer > 0){
+        tnk.sefer--;
+        tnk.perek = tnk.psukim[tnk.sefer].length;
+        tnk.pasuk = tnk.psukim[tnk.sefer][tnk.perek-1][0];
+      }
+    }
+  }
+  tnk.book = tnk.seferim[tnk.sefer];
+}
+
+function next(){
+  if (tnk.pasuk < tnk.psukim[tnk.sefer][tnk.perek-1][0]){
+    tnk.pasuk++;
+  } else {
+    if (tnk.perek < tnk.psukim[tnk.sefer].length){
+      tnk.perek++;
+      tnk.pasuk = 1;
+    } else {
+      if (tnk.sefer < tnk.seferim.length-1){
+        tnk.sefer++;
+        tnk.perek = 1;
+        tnk.pasuk = 1;
+      }
+    }
+  }
+  tnk.book = tnk.seferim[tnk.sefer];
+}
+
+tnk.book = tnk.seferim[tnk.sefer];
+
+const i = document.getElementById('verse');
+i.addEventListener('change',()=> upd(i.value));
+upd(i.value);
+
+
+function upd(d){
+  fetchData(d,'heb-content','source');
+  fetchData(d,'eng-content','translation');
+}
+
+function fetchData(ref,id,lang){
+  const url = `https://www.sefaria.org/api/v3/texts/${ref}?version=${lang}`;
   const options = {method: 'GET', headers: {accept: 'application/json'}};
   fetch(url, options)
     .then(res => res.json())
-    .then(json => loadData(json))
-    .catch(err => console.error(err));
+    .then(json => {
+      loadData(json,id)
+    }).catch(err => console.error(err));
 }
 
-function loadData(data){
-  const heb = document.getElementById('heb-content');
-  const eng = document.getElementById('eng-content');
-  heb.innerHTML = data.versions[0].text;
-  //eng.innerHTML = data.text.join(' ');
+function loadData(data,id){
+  if (data && data.versions && data.versions[0] && data.versions[0].text){
+    const e = document.getElementById(id);
+    console.log(data);
+    e.innerHTML = data.versions[0].text;
+  }
 }
 
 //https://developers.sefaria.org/reference/get-v3-texts
