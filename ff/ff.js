c6a181e5544377cc4d9a7e3cc51b4dec529aad80
diasgc <gustavocdias@me.com>
2025-12-01 18:15:28 +0000

    upd ff

--- ff/ff.js
+++ ff/ff.js
@@ -3,105 +3,131 @@
 var ffmpeg = null;
 var loadBtn = null;
 var transcodeBtn = null;
-var logDiv = null;
 var videoEl = null;
-var tryMultiThread = true;
-const baseURLFFMPEG = 'https://unpkg.com/@ffmpeg/ffmpeg@0.12.6/dist/umd';
-const baseURLCore = 'https://unpkg.com/@ffmpeg/core@0.12.3/dist/umd';
-const baseURLCoreMT = 'https://unpkg.com/@ffmpeg/core-mt@0.12.3/dist/umd';
 
-const toBlobURLPatched = async (url, mimeType, patcher) => {
+const v = {
+  ff:  '0.12.6', //0.12.15
+  c:   '0.12.3', //0.12.10
+  cmt: '0.12.3', //0.12.10
+}
+
+const ui = {
+  bLoad: document.getElementById('load-button'),
+  bTrans: document.getElementById('transcode-button'),
+  iFiles: document.getElementById('uploader'),
+  log: document.getElementById('log-div'),
+  video: document.getElementById('video-result'),
+  args: document.getElementById('ffargs'),
+  furl: document.getElementById('ffurl'),
+  loadFile: function(file){
+    if (file){
+      this.video.src = URL.createObjectURL(file);
+      this.video.load();
+      this.bTrans.removeAttribute('disabled');
+    }
+  }
+}
+
+const ffUtil = {
+  ffmpeg: null,
+  tryMultiThread: true,
+  ffVersion: '0.12.6',
+  ffCoreVersion: '0.12.3',
+  urlFF: `https://unpkg.com/@ffmpeg/ffmpeg@${v.ff}/dist/umd`,
+  urlCore: `https://unpkg.com/@ffmpeg/core@${v.c}/dist/umd`,
+  urlCoreMT: `https://unpkg.com/@ffmpeg/core-mt@${v.cmt}/dist/umd`,
+  toBlobURLPatched: async (url, mimeType, patcher) => {
     var resp = await fetch(url);
     var body = await resp.text();
     if (patcher) body = patcher(body);
     var blob = new Blob([body], { type: mimeType });
     return URL.createObjectURL(blob);
-};
-
-const toBlobURL = async (url, mimeType) => {
+  },
+  toBlobURL: async (url, mimeType) => {
     var resp = await fetch(url);
     var body = await resp.blob();
     var blob = new Blob([body], { type: mimeType });
     return URL.createObjectURL(blob);
-};
-
-const fetchFile = async (url) => {
+  },
+  fetchFile: async (url) => {
     var resp = await fetch(url);
     var buffer = await resp.arrayBuffer();
     return new Uint8Array(buffer);
-};
-
-const inputData = {
-    file: null,
-}
-
-document.getElementById('uploader').addEventListener('change', async (event) => {
-    inputData.file = event.target.files[0];
-    if (inputData.file) {
-        const video = document.getElementById('video-result');
-        video.src = URL.createObjectURL(inputData.file);
-        video.load();
-        const arrayBuffer = await inputData.file.arrayBuffer();
-        inputData.data = new Uint8Array(arrayBuffer);
-        transcodeBtn.removeAttribute('disabled');
-    }
-});
-
-const load = async () => {
-    loadBtn.setAttribute('disabled', true);
-    const ffmpegBlobURL = await toBlobURLPatched(`${baseURLFFMPEG}/ffmpeg.js`, 'text/javascript', (js) => js.replace('new URL(e.p+e.u(814),e.b)', 'r.workerLoadURL'));
+  },
+  load: async (onload) => {
+    const ffmpegBlobURL = await ffUtil.toBlobURLPatched(`${ffUtil.urlFF}/ffmpeg.js`, 'text/javascript', (js) => js.replace('new URL(e.p+e.u(814),e.b)', 'r.workerLoadURL'));
     await import(ffmpegBlobURL);
-    ffmpeg = new FFmpegWASM.FFmpeg();
-    ffmpeg.on('log', ({ message }) => {
-        logDiv.innerHTML = message;
-        console.log(message);
+    ffUtil.ffmpeg = new FFmpegWASM.FFmpeg();
+    ffUtil.ffmpeg.on('log', ({ message }) => {
+      ui.log.innerText += message + '\n';
+      console.log(message);
     });
     // check if SharedArrayBuffer is supported via crossOriginIsolated global var
     // https://developer.mozilla.org/en-US/docs/Web/API/crossOriginIsolated
-    if (tryMultiThread && window.crossOriginIsolated){
-        transcodeBtn.innerHTML = 'Transcode webm to mp4 (multi-threaded)';
-        await ffmpeg.load({
-            workerLoadURL: await toBlobURL(`${baseURLFFMPEG}/814.ffmpeg.js`, 'text/javascript'),
-            coreURL: await toBlobURL(`${baseURLCoreMT}/ffmpeg-core.js`, 'text/javascript'),
-            wasmURL: await toBlobURL(`${baseURLCoreMT}/ffmpeg-core.wasm`, 'application/wasm'),
-            workerURL: await toBlobURL(`${baseURLCoreMT}/ffmpeg-core.worker.js`, 'application/javascript'),
-        });
+    if (ffUtil.tryMultiThread && window.crossOriginIsolated){
+      await ffUtil.ffmpeg.load({
+        workerLoadURL: await ffUtil.toBlobURL(`${ffUtil.urlFF}/814.ffmpeg.js`, 'text/javascript'),
+        coreURL: await ffUtil.toBlobURL(`${ffUtil.urlCoreMT}/ffmpeg-core.js`, 'text/javascript'),
+        wasmURL: await ffUtil.toBlobURL(`${ffUtil.urlCoreMT}/ffmpeg-core.wasm`, 'application/wasm'),
+        workerURL: await ffUtil.toBlobURL(`${ffUtil.urlCoreMT}/ffmpeg-core.worker.js`, 'application/javascript'),
+      });
     } else {
-        transcodeBtn.innerHTML = 'Transcode webm to mp4 (single-threaded)';
-        await ffmpeg.load({
-            workerLoadURL: await toBlobURL(`${baseURLFFMPEG}/814.ffmpeg.js`, 'text/javascript'),
-            coreURL: await toBlobURL(`${baseURLCore}/ffmpeg-core.js`, 'text/javascript'),
-            wasmURL: await toBlobURL(`${baseURLCore}/ffmpeg-core.wasm`, 'application/wasm'),
+        await ffUtil.ffmpeg.load({
+          workerLoadURL: await ffUtil.toBlobURL(`${ffUtil.urlFF}/814.ffmpeg.js`, 'text/javascript'),
+          coreURL: await ffUtil.toBlobURL(`${ffUtil.urlCore}/ffmpeg-core.js`, 'text/javascript'),
+          wasmURL: await ffUtil.toBlobURL(`${ffUtil.urlCore}/ffmpeg-core.wasm`, 'application/wasm'),
         });
     }
     console.log('ffmpeg.load success');
-    //transcodeBtn.removeAttribute('disabled');
+    if (onload) onload();
+  },
+  transcodeFile: async (inExt, inData, outExt, strArgs) => {
+    const ffargs = strArgs.trim().split(' ');
+    await ffUtil.ffmpeg.writeFile(`input.${inExt}`, inData);
+    await ffUtil.ffmpeg.exec(['-i', `input.${inExt}`, ...ffargs, `output.${outExt}`]);
+    const data = await ffUtil.ffmpeg.readFile(`output.${outExt}`);
+    return data;
+  },
+  transcodeUrl: async (inUrl, outExt, strArgs) => {
+    const ffargs = strArgs.trim().split(' ');
+    await ffUtil.ffmpeg.exec(['-i', inUrl, ...ffargs, `output.${outExt}`]);
+    const data = await ffUtil.ffmpeg.readFile(`output.${outExt}`);
+    return data;
+  }
 }
 
-const tryFetchInputFile = async () => {
-    if (inputData.file) {
-        return await inputData.file.arrayBuffer();
-    } else {
-        return await fetchFile('https://raw.githubusercontent.com/ffmpegwasm/testdata/master/Big_Buck_Bunny_180_10s.webm');
-    }
-};
 
-const transcode = async () => {
-    transcodeBtn.setAttribute('disabled', true);
-    let args = document.getElementById('ffargs').innerText.split('\s+');
-    await ffmpeg.writeFile('input.webm', inputData.data);
-    await ffmpeg.exec(['-i', 'input.webm', ...args, 'output.mp4']);
-    const data = await ffmpeg.readFile('output.mp4');
-    videoEl.src = URL.createObjectURL(new Blob([data.buffer], { type: 'video/mp4' }));
+const inputData = {
+  file: null,
+  data: null,
+  extIn: 'webm',
+  extOut: 'mp4',
+  setFile: async function (file){
+    if (file){
+      this.file = file;
+      const ext = file.name.split('.').pop().toLowerCase();
+      this.extIn = ext;
+      const arrayBuffer = await inputData.file.arrayBuffer();
+      inputData.data = new Uint8Array(arrayBuffer);
+      ui.loadFile(inputData.file);
+    }
+  }
+}
+
+async function transcode(){
+  if (ui.furl.innerText.startsWith('http')){
+    await ffUtil.transcodeUrl(ui.furl.innerText, inputData.extOut, ui.args.innerText);
+  } else if (inputData.file){
+    await ffUtil.transcodeFile(inputData.extIn, inputData.data, inputData.extOut, ui.args.innerText);
+  } else {
+    ui.log.innerText = "No input file or URL specified.\n";
+  }
 }
 
 addEventListener("load", async (event) => {
-    loadBtn = document.querySelector('#load-button');
-    loadBtn.addEventListener('click', async () => await load());
-    loadBtn.removeAttribute('disabled');
-    transcodeBtn = document.querySelector('#transcode-button');
-    transcodeBtn.addEventListener('click', async () => await transcode());
-    logDiv = document.querySelector('#log-div');
-    videoEl = document.querySelector('#video-result');
-    console.log('window loaded');
+  ui.iFiles.addEventListener('change', async (event) => inputData.setFile(event.target.files[0]));
+  ui.bLoad.addEventListener('click', async () => await ffUtil.load(() => ui.bLoad.style.display = 'none'));
+  ui.bLoad.removeAttribute('disabled');
+  ui.bTrans.addEventListener('click', async () => await transcode());
+  console.log('window loaded');
 });
