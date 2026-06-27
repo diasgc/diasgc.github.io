41ae8b0374d77541f3ede3e9dba8f47b67223874
diasgc <gustavocdias@me.com>
2026-06-27 01:30:54 +0100

    upd

--- ubuntu-setup/sw/scripts/alias.sh
+++ ubuntu-setup/sw/scripts/alias.sh
@@ -1,4 +1,4 @@
-cat <<-EOF > ~/.bash_aliases
+cat <<EOF > ~/.bash_aliases
 alias reboot-uefi='systemctl reboot --firmware-setup'
 alias cls='clear'
 alias upgrade='sudo apt update && sudo apt upgrade'
@@ -8,7 +8,7 @@ alias sys-upgrade='sudo do-release-upgrade'
 alias gedit='gnome-text-editor'
 alias ged='gnome-text-editor'
 alias edit='gnome-text-editor'
-alias jpg2avif='for i in *.jpg; do a=$(basename "$i"); b=${a%.*}; ffmpeg -hide_banner -i "$a" "${b}.avif" && rm "$a"; done'
-alias jpeg2avif='for i in *.jpeg; do a=$(basename "$i"); b=${a%.*}; ffmpeg -hide_banner -i "$a" "${b}.avif" && rm "$a"; done'
-alias mp4tohevc='for i in *.mp4; do a=$(basename "$i"); b=${a%.*}; ffmpeg -hide_banner -i "$a" -c:v libx265 -crf 32 "${b}-HEVC.mp4" && rm "$a"; done'
+alias jpg2avif='for i in *.jpg; do ffmpeg -hide_banner -i "\${i}" "\${i/.jpg/.avif}" && rm "\${i}"; done'
+alias jpeg2avif='for i in *.jpeg; do ffmpeg -hide_banner -i "\${i}" "\${i/.jpeg/.avif}" && rm "\${i}"; done'
+alias mp4tohevc='for i in *.mp4; do ffmpeg -hide_banner -i "\${i}" -c:v libx265 -crf 32 "\${i/.mp4/-HEVC.mp4}" && rm "\${i}"; done'
 EOF
