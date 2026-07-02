7e8d9a5be313c03fd2885c5792919f4fe786fe98
diasgc <gustavocdias@me.com>
2026-07-02 23:28:15 +0100

    upd

--- ubuntu-setup/sw/scripts/alias.sh
+++ ubuntu-setup/sw/scripts/alias.sh
@@ -1,4 +1,4 @@
-cat <<EOF > ~/.bash_aliases
+cat <<'EOF' > ~/.bash_aliases
 alias reboot-uefi='systemctl reboot --firmware-setup'
 alias cls='clear'
 alias upgrade='sudo apt update && sudo apt upgrade'
@@ -10,45 +10,45 @@ alias ged='gnome-text-editor'
 alias edit='gnome-text-editor'
 
 function git-config(){
-    p=(\$(zenity --forms --text="Git Config" --add-entry="email" --add-entry="username" --add-combo="Scope" --combo-values="Local|Global" --separator=" "))
-    test -z "\${p[0]}" && echo "Email is required." && return 1
-    test -z "\${p[1]}" && echo "Username is required." && return
-    case "\${p[2],,}" in
-        local)  git config user.email "\${p[0]}" && git config user.name "\${p[1]}" ;;
-        global) git config --global user.email "\${p[0]}" && git config --global user.name "\${p[1]}" ;;
+    p=($(zenity --forms --text="Git Config" --add-entry="email" --add-entry="username" --add-combo="Scope" --combo-values="Local|Global" --separator=" "))
+    test -z "${p[0]}" && echo "Email is required." && return 1
+    test -z "${p[1]}" && echo "Username is required." && return
+    case "${p[2],,}" in
+        local)  git config user.email "${p[0]}" && git config user.name "${p[1]}" ;;
+        global) git config --global user.email "${p[0]}" && git config --global user.name "${p[1]}" ;;
     esac
     echo "Git config updated."
 }
 
 function ffmpeg-avif() {
-    test -z "\$1" && echo "Usage: ffmpeg-avif [-r remove original file] [<quality>] <file1> [file2 ...]" && return 1
-    case "\$1" in -r) rem=y; shift ;; esac
-    test -n "\$(grep -oP "\d+" <<<"\${1}")" && args+=(-crf "\${1}") && shift
-    args=(-c:v libaom-av1 -crf \${qual:-28})
-    for i in "\$@"; do
-        ffmpeg -hide_banner -i "\${i}" "\${args[@]}" "\${i/.*/.avif}" && case "\${rem,,}" in y) rm "\${i}" ;; esac
+    test -z "$1" && echo "Usage: ffmpeg-avif [-r remove original file] [<quality>] <file1> [file2 ...]" && return 1
+    case "$1" in -r) rem=y; shift ;; esac
+    test -n "$(grep -oP "\d+" <<<"${1}")" && args+=(-crf "${1}") && shift
+    args=(-c:v libaom-av1 -crf ${qual:-28})
+    for i in "$@"; do
+        ffmpeg -hide_banner -i "${i}" "${args[@]}" "${i/.*/.avif}" && case "${rem,,}" in y) rm "${i}" ;; esac
     done
 }
 
 function ffmpeg-hevc() {
-    test -z "\$1" && echo "Usage: ffmpeg-hevc [-r remove original file] [<quality>] <file1> [file2 ...]" && return 1
-    case "\$1" in -r) rem=y; shift ;; esac
-    test -n "\$(grep -oP "\d+" <<<"\${1}")" && args+=(-crf "\${1}") && shift
-    args=(-c:v libx265 -crf \${qual:-28})
-    for i in "\$@"; do
-        ffmpeg -hide_banner -i "\${i}" "\${args[@]}" "\${i/.*/-HEVC.mp4}" && case "\${rem,,}" in y) rm "\${i}" ;; esac
+    test -z "$1" && echo "Usage: ffmpeg-hevc [-r remove original file] [<quality>] <file1> [file2 ...]" && return 1
+    case "$1" in -r) rem=y; shift ;; esac
+    test -n "$(grep -oP "\d+" <<<"${1}")" && args+=(-crf "${1}") && shift
+    args=(-c:v libx265 -crf ${qual:-28})
+    for i in "$@"; do
+        ffmpeg -hide_banner -i "${i}" "${args[@]}" "${i/.*/-HEVC.mp4}" && case "${rem,,}" in y) rm "${i}" ;; esac
     done
 }
 
 function ffmpeg-cut() {
-    test -z "\$1" && echo "Usage: ffmpeg-cut [-r remove original file] <start_time>[-<end_time>] <file1> [file2 ...]" && return 1
-    case "\$1" in -r) rem=y; shift ;; esac
-    IFS='-' read -ra c <<< "\$1"; shift
+    test -z "$1" && echo "Usage: ffmpeg-cut [-r remove original file] <start_time>[-<end_time>] <file1> [file2 ...]" && return 1
+    case "$1" in -r) rem=y; shift ;; esac
+    IFS='-' read -ra c <<< "$1"; shift
     args=(-c copy)
-    test -n "\$(grep -oP "\d{2}:\d{2}" <<<"\${c[0]}")" && args+=(-ss "\${c[0]}")
-    test -n "\$(grep -oP "\d{2}:\d{2}" <<<"\${c[1]}")" && args+=(-to "\${c[1]}")
-    for i in "\$@"; do
-        ffmpeg -hide_banner -i "\${i}" "\${args[@]}" "\${i/.*/-cut.mp4}" && case "\${rem,,}" in y) rm "\${i}" ;; esac
+    test -n "$(grep -oP "\d{2}:\d{2}" <<<"${c[0]}")" && args+=(-ss "${c[0]}")
+    test -n "$(grep -oP "\d{2}:\d{2}" <<<"${c[1]}")" && args+=(-to "${c[1]}")
+    for i in "$@"; do
+        ffmpeg -hide_banner -i "${i}" "${args[@]}" "${i/.*/-cut.mp4}" && case "${rem,,}" in y) rm "${i}" ;; esac
     done
 }
 EOF
